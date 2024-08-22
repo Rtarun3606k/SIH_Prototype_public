b880d15cde22bc1499106c9f94588a0fcf4dba14
Rtarun3606k <r.tarunnayaka25042005@gmail.com>
2024-08-22 20:49:04 +0530

    changed type from string to text in admin and user model

--- prototype/backend/model/admin_model.py
+++ prototype/backend/model/admin_model.py
@@ -5,7 +5,7 @@ class ADMIN(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(50),unique=True,nullable=False)
     email = db.Column(db.String(50),unique=True,nullable=False)
-    password = db.Column(db.String(100),nullable=False)
+    password = db.Column(db.Text(),nullable=False)
 
     def __init__(self,username,email,password):
         self.username = username
--- prototype/backend/model/user_model.py
+++ prototype/backend/model/user_model.py
@@ -6,7 +6,7 @@ class USER(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(50),unique=True,nullable=False)
     email = db.Column(db.String(50),unique=True,nullable=False)
-    password = db.Column(db.String(100),nullable=False)
+    password = db.Column(db.Text(),nullable=False)
     created_at = db.Column(db.DateTime,nullable=False,default=datetime.now(timezone.utc))
     updated_at = db.Column(db.DateTime,nullable=False,default=datetime.now(timezone.utc),onupdate=datetime.now(timezone.utc))
 
